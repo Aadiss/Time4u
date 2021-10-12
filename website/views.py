e23484560300c861dbb674b498dbe150c9765038
Aadiss <adrian.stadnik00@gmail.com>
2021-10-12 18:55:08 +0200

    register form

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 import re
 from flask import render_template, redirect, url_for, request, Blueprint
+from flask.helpers import flash
 from flask_login import current_user
 
 views_bp = Blueprint("views", __name__)
@@ -7,6 +8,13 @@ views_bp = Blueprint("views", __name__)
 @views_bp.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        pass
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        print(username, email, password1, password2)
+        flash("Success!", category="success")
+        return redirect(url_for("views.home"))
     else:
         return render_template("index.html", user=current_user)
