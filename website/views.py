a549284a578e2ff7e1aa22377be7207c8365379d
Aadiss <adrian.stadnik00@gmail.com>
2021-10-14 12:23:23 +0200

    working registration

--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
 import re
 from flask import render_template, redirect, url_for, request, Blueprint
 from flask.helpers import flash
-from flask_login import current_user
+from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
+from . import db
 
 views_bp = Blueprint("views", __name__)
 
@@ -39,10 +41,12 @@ def home():
             flash("Passwords do not match!", category="error")
             return redirect(url_for("views.home"))
         else:
-            pass
-
-        print(username, email, password1, password2)
-        flash("Success!", category="success")
-        return redirect(url_for("views.home"))
+            new_user = User(email=email, login=username, password = generate_password_hash(password1, method="sha256"))
+            db.session.add(new_user)
+            db.session.commit()
+            print("user createt")
+            login_user(new_user, remember=True)
+            flash("Registration complete!", category="success")
+            return redirect(url_for("views.home"))
     else:
         return render_template("index.html", user=current_user)
