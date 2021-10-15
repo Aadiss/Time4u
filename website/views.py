d4dd54be1e7b4a56ff7462d12bd75a031bf70d0a
Aadiss <adrian.stadnik00@gmail.com>
2021-10-15 20:18:46 +0200

    debugging POST request in views.home

--- website/views.py
+++ website/views.py
@@ -11,42 +11,63 @@ views_bp = Blueprint("views", __name__)
 @views_bp.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
+        # register form
         username = request.form.get("username")
         email = request.form.get("email")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
-        email_in_db = User.query.filter_by(email=email).first()
-        username_in_db = User.query.filter_by(login=username).first()
+        #popup form for tasks
+        title = request.form.get("title")
+        priority = request.form.get("priority")
+        subject = request.form.get("subject")
+        content = request.form.get("content")
+        date_expired = request.form.get("date-expired")
 
-        if not username or not email or not password1 or not password2:
+        if (username or email or password1 or password2) or (title or priority or subject or content or date_expired):
+            if username and email and password1 and password2:
+                email_in_db = User.query.filter_by(email=email).first()
+                username_in_db = User.query.filter_by(login=username).first()
+
+                if email_in_db:
+                    flash("Email already exists!", category="error")
+                    return redirect(url_for("views.home"))
+                elif username_in_db:
+                    flash("Username already exist!", category="error")
+                    return redirect(url_for("views.home"))
+                elif "@" not in email:
+                    flash("Invalid email!", category="error")
+                    return redirect(url_for("views.home"))
+                elif len(username) < 6:
+                    flash("Usarname must be at least 6 signs!", category="error")
+                    return redirect(url_for("views.home"))
+                elif len(password1) < 6:
+                    flash("Password must be at least 6 signs!", category="error")
+                    return redirect(url_for("views.home"))
+                elif password1 != password2:
+                    flash("Passwords do not match!", category="error")
+                    return redirect(url_for("views.home"))
+                else:
+                    new_user = User(email=email, login=username, password = generate_password_hash(password1, method="sha256"))
+                    db.session.add(new_user)
+                    db.session.commit()
+                    print("user createt")
+                    login_user(new_user, remember=True)
+                    flash("Registration complete!", category="success")
+                    return redirect(url_for("views.home"))
+            
+            print("check popup form")
+            if title and priority and subject and content and date_expired:
+                print(title, priority, subject, content, date_expired)
+                flash("Filled corectly!", category="success")
+                return redirect(url_for("views.home"))
+            else:
+                print(title, priority, subject, content, date_expired)
+                flash("Fill all fields!", category="error")
+                return redirect(url_for("views.home"))
+        else:
             flash("Fill all fields!", category="error")
             return redirect(url_for("views.home"))
-        elif email_in_db:
-            flash("Email already exists!", category="error")
-            return redirect(url_for("views.home"))
-        elif username_in_db:
-            flash("Username already exist!", category="error")
-            return redirect(url_for("views.home"))
-        elif "@" not in email:
-            flash("Invalid email!", category="error")
-            return redirect(url_for("views.home"))
-        elif len(username) < 6:
-            flash("Usarname must be at least 6 signs!", category="error")
-            return redirect(url_for("views.home"))
-        elif len(password1) < 6:
-            flash("Password must be at least 6 signs!", category="error")
-            return redirect(url_for("views.home"))
-        elif password1 != password2:
-            flash("Passwords do not match!", category="error")
-            return redirect(url_for("views.home"))
-        else:
-            new_user = User(email=email, login=username, password = generate_password_hash(password1, method="sha256"))
-            db.session.add(new_user)
-            db.session.commit()
-            print("user createt")
-            login_user(new_user, remember=True)
-            flash("Registration complete!", category="success")
-            return redirect(url_for("views.home"))
+            
     else:
         return render_template("index.html", user=current_user)
