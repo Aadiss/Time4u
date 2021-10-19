812d0376edf0ab0371851c5ade207bebc6173a10
Aadiss <adrian.stadnik00@gmail.com>
2021-10-19 19:52:31 +0200

    heroku 1st step

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-DB_NAME = "postgresql://postgres:qaz@localhost/time4u"
+DB_NAME = "postgres://tqykwvgzcrxbck:4fe8b54617792dbea8d5e2e88992df30191d67cfec9b24dbba094cf405baadb1@ec2-34-249-247-7.eu-west-1.compute.amazonaws.com:5432/d8ne3r3rnot6gk"
 db = SQLAlchemy()
 
 def create_app():
