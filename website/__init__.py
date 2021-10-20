5ea22bf285d2d000fe290adfee4692d7af684599
Aadiss <adrian.stadnik00@gmail.com>
2021-10-20 11:22:40 +0200

    heroku deployed 10.20.2021

--- website/__init__.py
+++ website/__init__.py
@@ -2,29 +2,25 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-DB_NAME = "postgres://bcxnapvkrnuxdm:6aee2e94f13fc111b03bc400b66dc67613a58503164c85d21a19962f0220e08b@ec2-52-209-171-51.eu-west-1.compute.amazonaws.com:5432/d2h3br7ted57l9"
-db = SQLAlchemy()
+DB_NAME = "HEROKU_DB_URI"
 
-def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "qazwsxedc"
-    app.config["SQLALCHEMY_DATABASE_URI"] = DB_NAME
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "qazwsxedc"
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_NAME
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-    from .views import views_bp
-    from .auth import auth_bp
-    from .models import User
+from .views import views_bp
+from .auth import auth_bp
+from .models import User
 
-    login_manager = LoginManager()
-    login_manager.login_view = "auth.login"
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+login_manager.init_app(app)
 
-    app.register_blueprint(views_bp, url_prefix="/")
-    app.register_blueprint(auth_bp, url_prefix="/")
+app.register_blueprint(views_bp, url_prefix="/")
+app.register_blueprint(auth_bp, url_prefix="/")
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-
-    return app
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
